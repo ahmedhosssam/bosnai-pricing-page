97cf4c116735aca853bd3ed3a26ca176d00e6047
ahmedhossam <hoss4614@gmail.com>
2023-02-10 00:35:57 +0200

    finish header jsx syntax

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,22 @@
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <header>
+      <div className="left-header">
+        <img alt="logo" src="#" />
+        <div className="header-buttons">
+          <button>Product</button>
+          <button>Industires</button>
+          <button>Templates</button>
+          <button>Pricing</button>
+          <button>Reviews</button>
+        </div>
+      </div>
+      <div className="right-header">
+        <button className="login-button">Log in</button>
+        <button className="start-free-button">Start Free</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
